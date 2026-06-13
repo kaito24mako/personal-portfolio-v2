95605901517c5b9e1b38a427852d9dd1b008ddd1
Kaito Watanabe <kaitowatanabemcc@gmail.com>
2026-06-14 00:11:55 +1000

    initialise repo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Kaisei_Opti, Hina_Mincho } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const kaisei = Kaisei_Opti({
+  variable: "--font-kaisei",
+  weight: "400",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const hina = Hina_Mincho({
+  variable: "--font-hina",
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -25,9 +27,11 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${kaisei.variable} ${hina.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <main className="flex-1">{children}</main>
+      </body>
     </html>
   );
 }
