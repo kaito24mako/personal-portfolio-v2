5d5f08041dcaf4165770c4baaeaea6e77d63847f
Kaito Watanabe <kaitowatanabemcc@gmail.com>
2026-06-14 11:57:05 +1000

    complete home section for now

--- app/_sections/HomeSection.tsx
+++ app/_sections/HomeSection.tsx
@@ -3,9 +3,9 @@ import EdgeContainer from "@/_components/common/container/EdgeContainer";
 function HomeSection() {
   return (
     <EdgeContainer className="pt-30">
-      <h1 className="font-heading text-6xl md:text-7xl">I&apos;m Kaito</h1>
-      <h2 className="font-heading text-2xl md:text-3xl opacity-60">
-        With Passion in Web Development
+      <h1 className="font-heading text-6xl md:text-8xl">I&apos;m Kaito</h1>
+      <h2 className="font-heading text-2xl md:text-4xl opacity-60">
+        With passion in web development
       </h2>
     </EdgeContainer>
   );
